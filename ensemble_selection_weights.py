2b80fcac9564074d66fb96f8f9d744a3c60781da
nadiand <78037564+nadiand@users.noreply.github.com>
2024-12-11 09:13:38 +0100

    Move candidate to its own file

--- ensemble_selection_weights.py
+++ ensemble_selection_weights.py
@@ -10,43 +10,10 @@ from models import load_cifar10_models, load_cifar100_models, load_pascal_models
 from data import CIFARData, load_PascalVOC
 from conf_mat import ConfusionMatrix
 from evaluator import Evaluator
+from candidate import Candidate, SimpleProblem
 from diversity_metrics import pierson_correlation
 from torchmetrics import Accuracy
 from torch.nn import CrossEntropyLoss as CEL
-
-
-# Candidate class that contains the parameters and order
-class Candidate:
-    def __init__(self, voting_weights, generation):
-        self.voting_weights = voting_weights
-        self.fitness = None
-        self.generation = generation
-
-    # Evaluates the candidates parameters in the given order
-    def __call__(self):
-        return self.voting_weights
-
-    def set_fitness(self, fitness):
-        self.fitness = fitness
-
-class SimpleProblem:
-    def __init__(
-        self, model_lib, evaluator, pipeline, augment_mask=True, use_both_lighting=False
-    ):
-        self.model_lib = model_lib
-        self.evaluator = evaluator
-        self.pipeline = pipeline
-        self.augment_mask = augment_mask
-        self.use_both_lighting = use_both_lighting
-
-    def __call__(self, voting_weights, eval_type, sampler):
-        ensemble = []
-        for i, n in enumerate(voting_weights):
-            if n:
-                ensemble.append(self.model_lib[i])
-        norm_weights = [float(w)/sum(voting_weights) for w in voting_weights]
-        score = self.evaluator.run(ensemble, norm_weights, eval_type, sampler)
-        return score
 
 
 def initialize_population(population_size, N_models, threshold, history, g):
